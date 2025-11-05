2ebc9cd970fe2694707d7b01cf178fddf31bd6c7
david-gutsal <davidgutierrezsalvat@gmail.com>
2025-11-05 14:16:23 +0100

    [UPDATE DAWS] Agregado nuevo proyecto entregable : CastroNegro
    [UPDATE DAWC] Actualizado repaso usando DOM Y BOM

--- DesarrolloWebEntornoCliente/Dom/EjerciciosDOM_BOM/scripts/script.js
+++ DesarrolloWebEntornoCliente/Dom/EjerciciosDOM_BOM/scripts/script.js
@@ -6,4 +6,30 @@ function verPassword(){
         pw.setAttribute("type", "password");
     }
 }
-function setOnclick(){}
+
+function startCruz(){
+    let table = document.getElementsByTagName("table")[0];
+    table.setAttribute("class", "startGame");
+    let win1 = [1,1,1,0,0,0,0,0,0];
+    let win2 = [0,0,0,1,1,1,0,0,0];
+    let win3 = [0,0,0,0,0,0,1,1,1];
+    let win4 = [1,0,0,1,0,0,1,0,0];
+    let win5 = [0,1,0,0,1,0,0,1,0];
+    let win6 = [0,0,1,0,0,1,0,0,1];
+    let win7 = [1,0,0,0,1,0,0,0,1];
+    let win8 = [0,0,1,0,1,0,1,0,0];
+
+    let player1 = [];
+    let player2 = [];
+    let exit = false;
+    do{
+
+
+    exit = checkStade(player1, player2);
+    }while(exit);
+
+
+}
+function checkStade(player1, player2){
+     
+}
