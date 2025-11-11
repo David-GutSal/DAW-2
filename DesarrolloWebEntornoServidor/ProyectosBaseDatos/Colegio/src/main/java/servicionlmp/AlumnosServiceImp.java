53990f41964196efcb97d77831eb1836add39664
david-gutsal <davidgutierrezsalvat@gmail.com>
2025-11-11 14:07:15 +0100

    [UPDATE DAWS] Actualizado el proyecto donde se hace conexion a base de datos
    [ADD DIW] Agregado el proyecto entregalbe Footer, falta comentar el codigo
    
    [UPDATE DAWC] Agregado ejercicio manejando eventos

---
+++ DesarrolloWebEntornoServidor/ProyectosBaseDatos/Colegio/src/main/java/servicionlmp/AlumnosServiceImp.java
@@ -0,0 +1,19 @@
+package servicionlmp;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.IAlumnosDAO;
+import daolmp.AlumnosDAOImpl;
+import dto.AlumnoDTO;
+import servicios.IAlumnosService;
+
+public class AlumnosServiceImp implements IAlumnosService {
+	
+	@Override
+	public ArrayList<AlumnoDTO> obtenerAlumnos() throws SQLException {
+		IAlumnosDAO alumnos = new AlumnosDAOImpl();
+		return alumnos.obtenerTodosAlumnos();
+	}
+
+}
