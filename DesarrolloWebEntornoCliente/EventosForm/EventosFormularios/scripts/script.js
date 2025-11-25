5de0825a5e2ef680950113e78d4f5df491328c39
david-gutsal <davidgutierrezsalvat@gmail.com>
2025-11-25 14:13:58 +0100

    [UPDATE DAWC] Actualizado ejericico de repaso

--- DesarrolloWebEntornoCliente/EventosForm/EventosFormularios/scripts/script.js
+++ DesarrolloWebEntornoCliente/EventosForm/EventosFormularios/scripts/script.js
@@ -24,6 +24,15 @@ function cargarEventos() {
   document
     .getElementById("cFecha")
     .addEventListener(`click`, comprobarFecha, false);
+  document
+    .getElementById("cCorreo")
+    .addEventListener(`click`, comprobarCorreo, false);
+  document
+    .getElementById("cPalabras")
+    .addEventListener(`click`, comprobarPalabra, false);
+  document
+    .getElementById("cDNI")
+    .addEventListener(`click`, comprobarDNI, false);
 
 }
 
@@ -65,4 +74,16 @@ function comprobarFecha(){
   let fecha = prompt("Introduce una fecha");
   let patron = /\d{2}[-|/]{1}\d{2}[-|/]{1}[\d{4}|\d{2}]/;
   alert(patron.test(fecha));
+}
+
+function comprobarCorreo(){
+  let correo = prompt("Introduce un correo");
+  let patron = /[a-z]{1,}@gmail[.][es|com|net|org]{1}/;
+  alert(patron.test(correo));
+}
+
+function comprobarPalabra(){
+  let texto = prompt("Escibe un texto");
+  let patron = /\w{0,}[paca|vaca|baca|caca]{1,}\w{0,}/;
+  alert(patron.exec(texto));
 }
