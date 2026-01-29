62011019b4a15a58d77b2b7816d241750fd55237
david-gutsal <davidgutierrezsalvat@gmail.com>
2026-01-29 14:05:21 +0100

    [ADD DAWC] Agregado proyecto de vue usando vite
    [ADD FC] Agregado proyecto a eclipse para Práctica Cifrado HTTPs

---
+++ DesarrolloWebEntornoServidor/ProyectosBaseDatos/PracticaSeguridad/src/test/java/com/daw/seguridad/PracticaSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
