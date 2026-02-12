517a2a6720f087af99c7248c06735f93bbbae963
David-GutSal <davidgutierrezsalvat@gmail.com>
2026-02-12 21:59:12 +0100

    merge

--- DesarrolloWebEntornoCliente/Vue/Vite/practica_2/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const app = createApp(App);
-app.use(store);
-
-app.mount('#app');
